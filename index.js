699944633e3468961cf3c54d6264cb880fbd787c
Ralvin Adrian <itsmesteins@gmail.com>
2023-12-07 21:53:34 +0700

    removed wrong function to receive csv file

--- index.js
+++ index.js
@@ -34,42 +34,42 @@ app.get('/insert-data', async (req, res) => {
 // import importData from 'insert-data.js'
 
 // Handle CSV file upload and data insertion
-// app.post('/upload', upload.single('csvFile'), async (req, res) => {
-//     try {
-//         if (!req.file) {
-//             return res.render('/insert-data', { errMessage: "No file uploaded!" });
-//         }
+app.post('/upload', async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.render('/insert-data', { errMessage: "No file uploaded!" });
+        }
 
-//         // Process the uploaded CSV file
-//         const fileBuffer = req.file.buffer.toString('utf-8');
-//         const rows = fileBuffer.split('\n');
+        // Process the uploaded CSV file
+        const fileBuffer = req.file.buffer.toString('utf-8');
+        const rows = fileBuffer.split('\n');
 
-//         // Assuming the CSV header is present in the first row
-//         const header = rows[0].split(';');
+        // Assuming the CSV header is present in the first row
+        const header = rows[0].split(';');
 
-//         // Create a map to store column indexes
-//         const columnIndexMap = {};
-//         header.forEach((col, index) => {
-//             columnIndexMap[col.trim()] = index;
-//         });
+        // Create a map to store column indexes
+        const columnIndexMap = {};
+        header.forEach((col, index) => {
+            columnIndexMap[col.trim()] = index;
+        });
 
-//         // Process each data row
-//         for (let i = 1; i < rows.length; i++) {
-//             const row = rows[i].split(';');
-//             const rowData = {};
+        // Process each data row
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i].split(';');
+            const rowData = {};
 
-//             // Map data to column names
-//             Object.keys(columnIndexMap).forEach((col) => {
-//                 rowData[col] = row[columnIndexMap[col]].trim();
-//             });
+            // Map data to column names
+            Object.keys(columnIndexMap).forEach((col) => {
+                rowData[col] = row[columnIndexMap[col]].trim();
+            });
 
-//             // Insert data into MySQL tables
-//             await importData(rowData);
-//         }
+            // Insert data into MySQL tables
+            await importData(rowData);
+        }
 
-//         return res.status(200).send('CSV file processed and data inserted into MySQL tables.');
-//     } catch (error) {
-//         console.error('Error processing CSV file:', error);
-//         return res.status(500).send('Internal Server Error.');
-//     }
-// });
+        return res.status(200).send('CSV file processed and data inserted into MySQL tables.');
+    } catch (error) {
+        console.error('Error processing CSV file:', error);
+        return res.status(500).send('Internal Server Error.');
+    }
+});
